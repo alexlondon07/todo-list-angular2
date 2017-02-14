8411fe8ba5e30ed372cc7acc9df0e16140323b31
Alexander Londoño Espejo <alexlondon07@gmail.com>
2017-02-13 21:55:14 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    newTodo: string;
+    todos: any;
+    todoObj: any;
+
+    constructor() {
+      this.newTodo = '';
+      this.todos = [];
+    }
+
+    addTodo(event) {
+      this.todoObj = {
+        newTodo: this.newTodo,
+        completed: false
+      }
+      this.todos.push(this.todoObj);
+      this.newTodo = '';
+      event.preventDefault();
+    }
+
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    }
+
+    deleteSelectedTodos() {
+      for (let i = (this.todos.length -1); i > -1; i--) {
+        if (this.todos[i].completed) {
+          this.todos.splice(i, 1);
+        }
+      }
+    }
 }
